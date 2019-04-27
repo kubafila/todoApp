9d1bca6e599c04aeba9972cc0e221ff2c001bd72
Kuba Fila <kontakt@kubafila.pl>
2019-04-27 22:14:15 +0200

    Tag editor now works

---
+++ models/tag.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Tag = mongoose.model('Tag', mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 255
+    },
+    color: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 35
+    }
+}));
+
+function validateTag(tag) {
+    const schema = {
+        name: Joi.string().min(1).max(255).required(),
+        color: Joi.string().min(1).max(35).required(),
+    };
+
+    return Joi.validate(Tag, schema);
+}
+
+exports.Tag = Tag;
+exports.validateTag = validateTag;
---
+++ routes/tagsRoutes.js
@@ -0,0 +1,54 @@
+const {Tag, validateTag} = require('../models/tag');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const tags = await Tag.find().sort('name');
+    res.send(tags);
+})
+
+router.get('/:id', async (req, res) => {
+    const tag = await Tag.findById(req.params.id);
+    if (!tag) return res.status(404).send('The tag with the given ID was not found.');
+    else res.send(tag);
+})
+
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    const {error} = validateTag(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let tag = new Tag({
+        name: req.body.name,
+        color: req.body.color
+    });
+    tag = await tag.save();
+
+    res.send(tag);
+});
+
+router.put('/:id', async (req, res) => {
+    const {error} = validateTag(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const tag= await Tag.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        color: req.body.color
+    }, {
+        new: true
+    });
+
+    if (!tag) return res.status(404).send('The tag with the given ID was not found.');
+
+    res.send(tag);
+});
+
+router.delete('/:id', async (req, res) => {
+    const tag = await Tag.findByIdAndRemove(req.params.id);
+
+    if (!tag) return res.status(404).send('The tag with the given ID was not found.');
+
+    res.send(tag);
+});
+
+module.exports = router;
