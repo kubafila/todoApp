09c927d307f757051a17f2dca58a2c13121c365a
Kuba Fila <kontakt@kubafila.pl>
2019-04-29 23:33:21 +0200

    Info in Polish

--- routes/auth.js
+++ routes/auth.js
@@ -11,10 +11,10 @@ router.post('/', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send('Invalid email or password.');
+  if (!user) return res.status(400).send('Nie znaleziono emaila lub hasła');
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
-  if (!validPassword) return res.status(400).send('Invalid email or password.');
+  if (!validPassword) return res.status(400).send('Nie znaleziono emaila lub hasła');
 
   const token = user.generateAuthToken();
   res.send(token);
