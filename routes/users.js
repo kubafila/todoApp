941998b90628d449ecda4a53d8a27bfab097ac9b
Kuba Fila <kontakt@kubafila.pl>
2019-04-30 00:09:43 +0200

    More Human error ;)

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.get('/me', auth, async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).send('Błąd przy sprawdzaniu danych, najpewniej użycie niedozwolonego znaku specjalnego w haśle');
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('Konto już istnieje');
